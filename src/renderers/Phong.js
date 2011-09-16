21696231702124d225b1227df35612d2c77912df
Bartek Drozdz <bartek@everyday3d.com>
2011-09-16 16:25:35 +0200

    Removed a log call.

--- src/renderers/Phong.js
+++ src/renderers/Phong.js
@@ -1,5 +1,5 @@
 J3D.Phong = function(){
-	J3D.Shader.call( this, "Phong", J3D.ShaderSource.PhongVertex, J3D.ShaderSource.PhongFragment );
+    J3D.Shader.call(this, "Phong", J3D.ShaderSource.PhongVertex, J3D.ShaderSource.PhongFragment);
     this.uColor = J3D.Color.white;
     this.uSpecularIntensity = 0;
     this.uShininess = 0;
@@ -16,18 +16,18 @@ J3D.Phong.prototype.setup = function(shader){
     this.uHasColorSampler = (this.uColorSampler != null);
     
     for (var s in shader.uniforms) {
-		if (this[s] != null) {
+        if (this[s] != null) {
             J3D.ShaderUtil.setUniform(s, shader, this);
             
             if (!this.logd) {
-                console.log(s)
+                //console.log(s)
             }
         }
     }
-	
-	 if (!this.logd) {
-                console.log("-----: " + this.uColorSampler + " - " + this.uHasColorSampler);
-            }
-	
-	this.logd = true;
+    
+    if (!this.logd) {
+        //console.log("-----: " + this.uColorSampler + " - " + this.uHasColorSampler);
+    }
+    
+    this.logd = true;
 }
