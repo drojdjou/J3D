9a58087f5ae0c50a505819d817424a5d5a38714e
Bartek Drozdz <bartek@everyday3d.com>
2011-09-29 16:59:10 +0200

    Film noise function added to shader include.

--- src/engine/ShaderSource.js
+++ src/engine/ShaderSource.js
@@ -316,6 +316,13 @@ J3D.ShaderSource.CommonFilterInclude = [
 	"#endif",
 
 	"uniform float uTime;",
+
+	"float whiteNoise(vec2 uv, float scale) {",
+	"float x = (uv.x + 0.2) * (uv.y + 0.2) * (10000.0 + uTime);",
+	"x = mod( x, 13.0 ) * mod( x, 123.0 );",
+	"float dx = mod( x, 0.005 );",
+	"return clamp( 0.1 + dx * 100.0, 0.0, 1.0 ) * scale;",
+	"}",
 ""].join("\n");
 
 J3D.ShaderSource.CommonInclude = [
--- src/shaders/includes/CommonFilterInclude.glsl
+++ src/shaders/includes/CommonFilterInclude.glsl
@@ -4,3 +4,11 @@ precision highp float;
 #endif
 
 uniform float uTime;
+
+float whiteNoise(vec2 uv, float scale) {
+	// from Three.js / film shader
+	float x = (uv.x + 0.2) * (uv.y + 0.2) * (10000.0 + uTime);
+	x = mod( x, 13.0 ) * mod( x, 123.0 );
+	float dx = mod( x, 0.005 );
+	return clamp( 0.1 + dx * 100.0, 0.0, 1.0 ) * scale;
+}
